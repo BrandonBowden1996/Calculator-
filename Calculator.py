699fefb9b4cddb4207491969f408ef7931b72ac8
Brandon Bowden <BrandonBowden1996@outlook.com>
2021-05-31 21:31:52 +0100

    Updating the GUI

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,21 @@
 import tkinter
 
+
 window = tkinter.Tk()
 
-window.mainloop()
+#greeting = tkinter.Label(text="Hello, Tkinter")
 
+
+
+label = tkinter.Label(text="Hello, Tkinter")
+
+label = tkinter.Label(
+    text="Hello, Tkinter",
+    foreground="white", #Set the text color to white
+    background="black" # Set the background color to black
+)
+
+#greeting.pack()
+label.pack()
+
+window.mainloop() 
