2f65b749feb0febfa840653df8218ba74786f9de
Brandon Bowden <BrandonBowden1996@outlook.com>
2021-06-06 21:41:40 +0100

    Updating GUI to size window, add title & prevent from resizing

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,12 @@
-import tkinter
+import tkinter #importing the library for tkinter
 
 
-window = tkinter.Tk()
+
+window = tkinter.Tk() #creating an instance of tkinter
+
+window.geometry("312x324") #setting the GUIs size
+window.resizable(0, 0)
+window.title("Calculator")
 
 #greeting = tkinter.Label(text="Hello, Tkinter")
 
@@ -15,6 +20,23 @@ label = tkinter.Label(
     background="black" # Set the background color to black
 )
 
+def printerTextButton():
+    label = tkinter.label(
+        text="Number 8",
+        foreground="white",
+        background="black"
+    )
+
+button = tkinter.Button(
+    text="Click me!",
+    width=25,
+    height=5,
+    bg="white",
+    fg="orange",
+    command= printerTextButton,
+)
+
+button.pack()
 #greeting.pack()
 label.pack()
 
